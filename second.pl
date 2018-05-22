1d5579dcb93ce3684371b42358c99e25a4401870
Sergey <gavrin_s@mail.ru>
2018-05-22 14:49:34 +0700

    final

--- second.pl
+++ second.pl
@@ -3,14 +3,31 @@ use 5.010;
 use utf8;
 
 my $fname = 'access.log';
+my $typefile = '';
+
+my $fh;
+my $exec;
 
 my @items;
 
 
-open(my $fh, '<', $fname) or die "everything is bad'$fname' $!";
+if ($typefile eq '') {
+	$exec = $fname;
+	$typefile = '<';
+} elsif ($typefile eq 'bz2') {
+	$exec = "bzip2 -d \"${fname}\" --stdout";
+	$typefile = '-|';
+} elsif ($typefile eq 'gz') {
+	$exec = "gzip -d \"${fname}\" --stdout";
+	$typefile = '-|';
+}
+
+
+
+open(my $fh, $typefile, $exec) or die "everything is bad'$fname' $!";
 while (my $line = <$fh>)
 {
-	 if ($line =~ /^(\d+\.\d+\.\d+\.\d+)\s+([^\s])+\s+([^\s]+)\s+\[([^\s\]]+)\s+([^\s\]]+)\]\s+"(?:(get|head|post)\s+)?([^"]+)"\s+(\d+)\s+\d+\s+"([^"]+)"\s+"([^"]+)"/) {
+	 if ($line =~ /^(\d+\.\d+\.\d+\.\d+)\s+([^\s])+\s+([^\s]+)\s+\[([^\s\]]+)\s+([^\s\]]+)\]\s+"(?:(GET|HEAD|POST)\s+)?([^"]+)"\s+(\d+)\s+\d+\s+"([^"]+)"\s+"([^"]+)"/) {
             push @items, {
                     "ip" => $1,
                     "blank" => $2,
@@ -22,7 +39,7 @@ while (my $line = <$fh>)
                     "status" => $8,
                     "referer" => $9,
                     "user-agent" => $10,
-		    "request" => $line,
+		    "record" => $line,
 		    "danger" => 0		
                 };
         }
@@ -32,26 +49,46 @@ while (my $line = <$fh>)
 
 foreach my $item (@items)
     {
-	if ($$item{'request'} == "91.196.50.33")
+	# first
+	if ($$item{'method'} eq "")
 	{
-		$$item{'danger'} ++	
+		$$item{danger}++
 	}
-	#if ($$item{'timezone'} == '+0200')
-	#{
-	#	print "$$item{'timezone'} \n";
-	#}
-     
+
+	#second
+	if (index($$item{'request'}, "x0") != -1)
+	{
+		$$item{danger}++
+	}
+
+	#third
+	if ($$item{status} == 400)
+	{
+		$$item{danger}++;
+	}   
+	
+	#fourth
+	if (index($$item{'user-agent'}, "Python") != -1)
+	{
+		$$item{danger}++;
+	}
+	
+	#fifth
+	if ($$item{'ip'} eq "192.99.119.226")
+	{
+		$$item{danger}++;
+	}
     }
 
-print "$items[0]{danger} \n";
+# print "$items[0]{danger} \n";
 
 
 my $i = 0;
-foreach my $j (sort {$a->{danger} <=> $b->{danger}} @items)
+foreach my $j (sort {$b->{danger} <=> $a->{danger}} @items)
 {
-	print "$$j{danger} \n";
+	print "$$j{danger}, $$j{record} \n";
 	$i++;
-	if ($i>=5)
+	if ($i>=50)
 	{
 		last;
 	}
